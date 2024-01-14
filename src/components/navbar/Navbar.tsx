767c4f00c41605bdb01405d1657ece2df406c929
andresdanielmtz <autummata@gmail.com>
2024-01-14 16:16:23 -0700

    Fixed portfolio light mode inconsistencies

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { ToggleLanguage } from "./ToggleLang";
-
+import "./Navbar.css"
 export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -52,8 +52,8 @@ export default function Navbar() {
             id="navbar-sticky"
           >
             <ul
-              style={{ backgroundColor: "rgba(7, 7, 7, 1)" }}
-              className="bg-black md:order-2 opacity-100 md:bg-opacity-100 md:bg-black md:bg-slate-900 flex flex-col p-4 md:p-0 mt-4 font-medium bg-opacity-100 dark:bg-opacity-100 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            id = "navbar-sticky"
+              className="bg-slate-50 md:order-2 opacity-100 md:bg-opacity-100 md:bg-slate-50 md:bg-slate-50 flex flex-col p-4 md:p-0 mt-4 font-medium bg-opacity-100 dark:bg-opacity-100 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-slate-50 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
             >
               <li>
                 <Link to="/" onClick={toggleMobileMenu}>
